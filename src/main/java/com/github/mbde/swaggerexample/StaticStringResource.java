061242ff8b0e96c5f458984312219c4463410ceb
markus.decke <markus.decke@idealo.de>
2015-01-12 15:10:49 +0100

    swagger annotations on rest classes for documentation

--- src/main/java/com/github/mbde/swaggerexample/StaticStringResource.java
+++ src/main/java/com/github/mbde/swaggerexample/StaticStringResource.java
@@ -4,12 +4,16 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+
 /**
  * Created by markus.decke on 12.01.15.
  */
-
+@Api(value ="/string/static")
 public class StaticStringResource {
 
+    @ApiOperation( value ="hello world")
     @GET
     @Path("/hello")
     public String getStaticHelloWorld(){
@@ -17,6 +21,7 @@ public class StaticStringResource {
     }
 
 
+    @ApiOperation( value ="identity function")
     @GET
     @Path("{string}")
     public String getStatic(@PathParam("string") String string){
